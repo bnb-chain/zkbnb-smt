528021957c2d12221803046eb47d7bb12bcf565e
j75689 <j75689@gmail.com>
2022-07-13 09:59:32 +0800

    feat: add InitializeVersion option

--- options.go
+++ options.go
@@ -3,6 +3,12 @@ package bsmt
 // Option is a function that configures SMT.
 type Option func(*BASSparseMerkleTree)
 
+func InitializeVersion(version Version) Option {
+	return func(smt *BASSparseMerkleTree) {
+		smt.version = version
+	}
+}
+
 func BatchSizeLimit(limit int) Option {
 	return func(smt *BASSparseMerkleTree) {
 		smt.batchSizeLimit = limit

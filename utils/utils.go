d897048379f116eaf7287f57bba4b6b7720d4be9
j75689 <j75689@gmail.com>
2022-08-30 08:27:37 +0800

    docs: add LICENSE

--- utils/utils.go
+++ utils/utils.go
@@ -1,3 +1,8 @@
+// Copyright 2022 bnb-chain. All Rights Reserved.
+//
+// Distributed under MIT license.
+// See file LICENSE for detail or copy at https://opensource.org/licenses/MIT
+
 package utils
 
 import (

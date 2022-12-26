cd24f6f2be0db434febb5921aa01a0bcab884e1c
qct <tango.qct@gmail.com>
2022-12-26 15:29:42 +0400

    Add scenario to trigger rollback bug

--- smt_test.go
+++ smt_test.go
@@ -521,6 +521,7 @@ func testRollbackRecovery(t *testing.T, hasher *Hasher, dbInitializer func() (da
 		t.Fatal(err)
 	}
 	version1, err = smt2.Commit(nil)
+	rootVer1 := smt2.Root()
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -563,8 +564,14 @@ func testRollbackRecovery(t *testing.T, hasher *Hasher, dbInitializer func() (da
 	}
 
 	err = smt2.Rollback(version1)
+	smt2.Set(key1, val1)
+	_, err = smt2.Commit(nil)
 	if err != nil {
 		t.Fatal(err)
+	}
+	rootVer2 := smt2.Root()
+	if !bytes.Equal(rootVer1, rootVer2) {
+		t.Fatal("Roots at version1 and version2 are NOT equal")
 	}
 
 	err = smt.Rollback(version1)
@@ -593,6 +600,7 @@ func Test_BASSparseMerkleTree_RollbackAfterRecovery(t *testing.T) {
 	for _, env := range prepareEnv(t) {
 		t.Logf("test [%s]", env.tag)
 		testRollbackRecovery(t, env.hasher, env.db)
+		break
 	}
 }
 
